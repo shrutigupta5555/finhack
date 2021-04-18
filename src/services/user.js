dae1e5c550a639ea135b8eb82ca8b8c5bfdfd1b8
Sagnik Mandal <sonalisagnik123@gmail.com>
2021-04-18 16:36:39 +0530

    Add: Profile API Functions

--- src/services/user.js
+++ src/services/user.js
@@ -11,12 +11,29 @@ class UserService {
         return response.data.clients;
       });
   }
+  getProfile() {
+    return axios
+      .get(API_URL + "profile", { headers: authHeader() })
+      .then(response => {
+        return response.data;
+      });
+  }
   addClients(client_name, client_email, client_phone_no) {
     return axios
       .post(API_URL + "clients/add", {
       client_name,
       client_email,
       client_phone_no
+      }, { headers: authHeader() })
+      .then(response => {
+        return response.data;
+      });
+  }
+  updateProfile(name, password) {
+    return axios
+      .post(API_URL + "profile/update", {
+      name,
+      password
       }, { headers: authHeader() })
       .then(response => {
         return response.data;
