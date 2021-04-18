a42569ba0c9b69722f4279d6e30e540838cad01d
Shruti <shrutigupta5555@gmail.com>
2021-04-18 16:56:31 +0530

    ADD: Profile Component

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,11 +1,34 @@
 import React from 'react'
 
+
 function MyProfile() {
     return (
         
         <div className="client-container">
-            <h1>My profile</h1>
+            <h3 className="h3-myp">My profile</h3>
+
+            
+            <section className="container my-profile-wrapper" >
+                
+                <div className="my-profile"> 
+                    <div className="head">
+                        <div className="circle"></div>
+                        <h2>Simon Max</h2>
+                        
+                    </div>
+                    <div className="info">
+                        <p>Email: <span>simonmax@outlook.com</span></p>
+                        <p>Phone Number: <span>+1 8080987236</span></p>
+                        <p>Payment ID <span>nwufnwenw</span></p>
+                    </div>
+                </div>
+                    
+            </section>
+
+       
         </div>
+
+        
     )
 }
 
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,23 +3,25 @@ import React, {useState} from 'react'
 
 function SideBar({selectRoute, selectedRoute}) {
 
-    const style = {
-        color : 'black'
+    const [clientRouteSideBar, setclientRouteSideBar] = useState(false)
+    const [myProfileSideBar, setmyProfileSideBar] = useState(true)
+
+    const active = {
+        color :'grey'
     }
 
     const notActive = {
-        color: 'grey'
+        color: 'black'
     }
-    
 
     return (
         <div className="sidebar" onClick={(e) => selectRoute(e)}>
 
             
 
-            <h3 style={style}>My Clients</h3>
-            <h3 style={notActive}>My Profile</h3>
-            <h3 style={notActive}>Logout</h3>
+            <h3 style={notActive}>My Clients</h3>
+            <h3 style={active} onClick={()=>setmyProfileSideBar(!myProfileSideBar)}>My Profile</h3>
+            <h3>Logout</h3>
 
             
             
